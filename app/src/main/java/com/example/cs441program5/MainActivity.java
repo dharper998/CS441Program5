6ff795b8920f9767a7e091060d669b43c86dc880
Drew Harper <dharper998@gmail.com>
2021-03-19 01:12:07 -0400

    Adding animation for text view based on accelerometer reading

--- app/src/main/java/com/example/cs441program5/MainActivity.java
+++ app/src/main/java/com/example/cs441program5/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.cs441program5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.Point;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.Display;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    private SensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        TextView text = findViewById(R.id.text);
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        SensorEventListener accelerometerListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if(text.getX() >= 50 && (-sensorEvent.values[0] < 0)){
+                    text.setX(text.getX() + (-sensorEvent.values[0] * 5));
+                } else if(text.getX() <= 850 && (-sensorEvent.values[0] > 0)){
+                    text.setX(text.getX() + (-sensorEvent.values[0] * 5));
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+            }
+        };
+
+        sensorManager.registerListener(accelerometerListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 }
