0b0dee88f2396617b925c07f10d8baa633a532ec
Drew Harper <dharper998@gmail.com>
2021-03-22 10:43:27 -0400

    Finalizing project. Implementing scrolling obstacle cars for user to avoid using gyroscope

--- app/src/main/java/com/example/cs441program5/MainActivity.java
+++ app/src/main/java/com/example/cs441program5/MainActivity.java
@@ -9,13 +9,17 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.Image;
 import android.os.Bundle;
+import android.renderscript.Sampler;
 import android.view.Display;
 import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private SensorManager sensorManager;
@@ -29,6 +33,17 @@ public class MainActivity extends AppCompatActivity {
         ImageView background2 = findViewById(R.id.imageView2);
         ImageView background3 = findViewById(R.id.imageView3);
 
+        ImageView ambulance = findViewById(R.id.ambulance);
+        ambulance.setX(130);
+
+        ImageView car2 = findViewById(R.id.car2);
+        car2.setX(450);
+
+        ImageView van = findViewById(R.id.van);
+        van.setX(290);
+
+        ImageView taxi = findViewById(R.id.taxi);
+        taxi.setX(580);
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
@@ -66,9 +81,25 @@ public class MainActivity extends AppCompatActivity {
                 background1.setTranslationY(translationY);
                 background2.setTranslationY(translationY - height/2);
                 background3.setTranslationY(translationY - height);
-
             }
         });
+        final ValueAnimator carAnimator = ValueAnimator.ofFloat(0.0f, 1.0f);
+        carAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        carAnimator.setInterpolator(new LinearInterpolator());
+        carAnimator.setDuration(10000L);
+        Random rand = new Random();
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+           @Override
+           public void onAnimationUpdate(ValueAnimator animation) {
+               final float progress = (float) animation.getAnimatedValue();
+               final float height = background1.getHeight();
+               car2.setTranslationY((height * 3 * progress) - height);
+               van.setTranslationY((height* 2 * progress) - height);
+               ambulance.setTranslationY((height * 4 * progress) - height);
+               taxi.setTranslationY((height * 5 * progress) - height);
+           }
+        });
+        carAnimator.start();
         animator.start();
 
     }
